a3bb7227291c8f8d979a1081bad7f7e71d983823
Sergiu elisei <ctinsergiu@gmail.com>
2023-01-19 18:19:36 +0200

    add workspaces

---
+++ apps/app1/src/pages/orgs.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Org() {
+  return <div>new page example 123</div>;
+}
--- src/pages/orgs.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Org() {
-  return <div>new page example</div>;
-}
