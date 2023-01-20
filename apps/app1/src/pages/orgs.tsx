57d3bb0fe53dbe6c3773fee18d72b38fa20ca338
Sergiu elisei <ctinsergiu@gmail.com>
2023-01-20 16:36:58 +0200

    update env var

--- apps/app1/src/pages/orgs.tsx
+++ apps/app1/src/pages/orgs.tsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Org() {
-  return <div>env var: {process.env.TEST_VARIABLE}</div>;
+  return <div>env var: {process.env.NEXT_PUBLIC_TEST_VARIABLE}</div>;
 }
